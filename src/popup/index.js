61f43d9d52dd08714b79590ffcb7f985ac03e97b
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-03 08:15:46 -0600

    made some comments to help with org

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,18 +1,22 @@
+//<====================IMPORTS=====================>
 import './index.css'
 import 'suneditor/dist/css/suneditor.min.css'
 import suneditor from 'suneditor'
 import plugins from 'suneditor/src/plugins'
 
+//<====================DOM OBJECTS==================>
 const templateOptionsElement = document.getElementById('template-options')
 const emptyTemplatesElement = document.getElementById('no-templates')
 const addTemplateButton = document.getElementById('add-template-button')
 const addTemplateModal = document.getElementById('add-template-modal')
 const closeTemplateModalButton = document.getElementById('close-button')
 
+//<====================FUNCTIONS=====================>
 const addTemplate = () => {
   addTemplateModal.showModal()
 }
 
+//<====================BUSINESS LOGIC================>
 suneditor.create('template-text-area', {
   plugins: plugins,
   buttonList: [
@@ -25,8 +29,9 @@ suneditor.create('template-text-area', {
     ['table', 'link', 'image', 'video'],
     ['fullScreen', 'showBlocks'],
     ['preview', 'print'],
+    ['save'],
   ],
-  height: 200,
+  height: 325,
 })
 
 if (templateOptionsElement.children.length === 0) {
@@ -35,5 +40,6 @@ if (templateOptionsElement.children.length === 0) {
   emptyTemplatesElement.hidden = true
 }
 
+//<===================EVENT LISTENERS================>
 addTemplateButton.addEventListener('click', () => addTemplate())
 closeTemplateModalButton.addEventListener('click', () => addTemplateModal.close())
