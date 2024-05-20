8006def516ea3723e0eb037438a567ac1a1347c6
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-20 11:18:48 -0600

    add interval to update clock per second

--- src/newtab/index.js
+++ src/newtab/index.js
@@ -1,5 +1,8 @@
-//<==================IMPORTS===============>
+//<==================IMPORTS===================>
 import './index.css'
 import { loadTimezones } from '../controllers/loadTimezones.js'
 
+//<===============STARTUP LOGIC================>
+//load up current time and timezones, set update interval
 loadTimezones()
+setInterval(loadTimezones, 1000)
