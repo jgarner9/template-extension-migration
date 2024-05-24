d4793da6f2fbd34f19a49537dd46e5605f900bf3
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-23 18:04:41 -0600

    added open and close logic for dialog modal

--- src/newtab/index.js
+++ src/newtab/index.js
@@ -10,3 +10,16 @@ setInterval(loadTimezones, 1000)
 
 //load checklist items in dashboard
 loadChecklist()
+
+//<==============EVENT LISTENERS==============>
+const addChecklistModal = document.getElementById('add-checklist-modal')
+const addChecklistButton = document.getElementById('add-checklist-button')
+const closeAddChecklistButton = document.getElementById('close-add-button')
+
+addChecklistButton.addEventListener('click', () => {
+  addChecklistModal.showModal()
+})
+
+closeAddChecklistButton.addEventListener('click', () => {
+  addChecklistModal.close()
+})
