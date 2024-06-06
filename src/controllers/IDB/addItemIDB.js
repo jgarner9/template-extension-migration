741801444fa007603094df382ae43d4e467c4872
Jason <63689065+jgarner9@users.noreply.github.com>
2024-06-05 19:37:23 -0600

    create addItemIDB function

--- src/controllers/IDB/addItemIDB.js
+++ src/controllers/IDB/addItemIDB.js
@@ -1,3 +1,27 @@
-const addItemIDB = () => {}
+const addItemIDB = (request, storeName, data) => {
+  request.addEventListener('success', (e) => {
+    const db = e.target.result
+
+    const objectStore = db.transaction(storeName, 'readwrite').objectStore(storeName)
+
+    objectStore.add(data)
+
+    db.close()
+  })
+
+  request.addEventListener('upgradeneeded', (e) => {
+    const db = e.target.result
+
+    const objectStore = db.createObjectStore(storeName, { keyPath: 'id' })
+
+    objectStore.transaction.oncomplete = (e) => {
+      const objectStore = db.transaction(storeName, 'readwrite').objectStore(storeName)
+
+      objectStore.add(data)
+
+      db.close()
+    }
+  })
+}
 
 export { addItemIDB }
